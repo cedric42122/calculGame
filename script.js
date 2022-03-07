5debcadf719f1474793e304a2f11d72b59e4ced4
cedric42122 <cedric.legat42@gmail.com>
2022-03-07 18:03:48 +0100

    Amélioration timer

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
-document.getElementById("gameZone").hidden = true;
+let gameZoneState = document.getElementById("gameZone");
+let gameStartState = document.getElementById("gameStart");
+
+gameZoneState.hidden = true;
+console.log("start");
+let time = 10;
 
 function gameStart() {
-  if (document.getElementById("gameZone").hidden = false)
-    return document.getElementById("gameStart").hidden = true; 
-  else (document.getElementById("gameStart").hidden = true)
-    return document.getElementById("gameZone").hidden = false;  
+  if ((gameZoneState.hidden = false))
+    return (gameStartState.hidden = true);
+  else gameStartState.hidden = true;
+  return (gameZoneState.hidden = false);
 }
 
-function randomIntFromInterval(min, max) { 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const rndInt1 = randomIntFromInterval(1, 10);
+const rndInt2 = randomIntFromInterval(1, 10);
+var result = rndInt1 + rndInt2;
+document.getElementById("num").textContent = rndInt1 + "+" + rndInt2;
+
+const timerElement = document.getElementById("timer");
+
+function decreaseTime() {
+  timerElement.innerText = "Il reste " + time + " secondes";
+  if (time > 0) time--;
+  else {
+    return (document.getElementById("timer").textContent =
+      "Vous avez mis trop de temps pour répondre !");
   }
-  
-  const rndInt1 = randomIntFromInterval(1, 10)
-  const rndInt2 = randomIntFromInterval(1, 10)
-  var result = rndInt1 + rndInt2;
-  document.getElementById('num').textContent = rndInt1 + "+" + rndInt2;
+}
 
-  let time = 10
-  const timerElement = document.getElementById("timer")
+function startTimer() {
+  setInterval(decreaseTime, 1000);
+}
 
-  function decreaseTime() {
-    timerElement.innerText = "Il reste "+ time + " secondes"
-    if(time > 0)
-    time--
-
-    else {
-      return document.getElementById("timer").textContent = "Vous avez mis trop de temps pour répondre !"
-    }
-  }
-
-  setInterval(decreaseTime, 1000)
 
 function answerAnalysis() {
   var answer = parseInt(document.getElementById("answer").value);
-    if (answer == result) {
-      return document.getElementById('answerText').textContent="C'est une bonne réponse !";
-    }
-    else {
-      return document.getElementById('answerText').textContent="Ce n'est pas la bonne réponse ...recommence !";
-    }
-  } 
+  if (answer == result) {
+    return (document.getElementById("answerText").textContent =
+      "C'est une bonne réponse !");
+  } else {
+    return (document.getElementById("answerText").textContent =
+      "Ce n'est pas la bonne réponse ...recommence !");
+  }
+}
 
 function pressReturn() {
   if (event.keyCode === 13) {
@@ -47,4 +54,4 @@ function pressReturn() {
   }
 }
 
-console.log("ppl");
+console.log("end");
